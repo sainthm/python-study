9d5616bbb671efd59e7966bb7cd7c42b3b9025f5
sainthm <sainthm@naver.com>
2022-08-02 00:05:51 +0900

    Add BAEKGOON Q1001

---
+++ BAEKJOON/Q1001.py
@@ -0,0 +1,13 @@
+# Q 1001.py
+
+# 문제
+# 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+
+Input = input().split(' ')
+A = int(Input[0])
+B = int(Input[1])
+print(A-B)
