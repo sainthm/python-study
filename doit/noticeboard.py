e5c2a1f29ba360f00e2ae9b21875bf91eddd5fad
sainthm <sainthm@naver.com>
2022-08-01 22:23:02 +0900

    add noticeboard.py example

---
+++ doit/noticeboard.py
@@ -0,0 +1,9 @@
+# noticeboard.py
+
+def getTotalPage(m,n ):
+    return m // n + 1
+
+print(getTotalPage(5, 10))
+print(getTotalPage(15, 10))
+print(getTotalPage(25, 10))
+print(getTotalPage(30, 10))
