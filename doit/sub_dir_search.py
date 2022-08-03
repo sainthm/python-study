84c5d9e2e77d1b0e6e680adc6d476d9cf87deb08
sainthm <sainthm@naver.com>
2022-08-04 01:13:53 +0900

    Add sub_dir_search.py example ver.2

--- doit/sub_dir_search.py
+++ doit/sub_dir_search.py
@@ -1,6 +1,12 @@
 # sub_dir_search.py
 
+import os
+
 def search(dirname):
+    filenames = os.listdir(dirname)
+    for filename in filenames:
+        full_filename = os.path.join(dirname, filename)
+        print(full_filename)
     print(dirname)
 
 search("C:/")
