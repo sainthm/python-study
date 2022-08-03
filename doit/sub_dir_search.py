f1464c7c75675ce520cbd3ced8f40a50e651a7f1
sainthm <sainthm@naver.com>
2022-08-04 01:17:33 +0900

    Add sub_dir_search.py example ver.3

--- doit/sub_dir_search.py
+++ doit/sub_dir_search.py
@@ -1,14 +1,21 @@
 # sub_dir_search.py
 
+from genericpath import isdir
 import os
 
 def search(dirname):
-    filenames = os.listdir(dirname)
-    for filename in filenames:
-        full_filename = os.path.join(dirname, filename)
-        ext = os.path.splitext(full_filename)[-1]
-        if ext == '.py':
-            print(full_filename)
-    # print(dirname)
+    try:
+        filenames = os.listdir(dirname)
+        for filename in filenames:
+            full_filename = os.path.join(dirname, filename)
+            if os.path.isdir(full_filename):
+                search(full_filename)
+            else:
+                ext = os.path.splitext(full_filename)[-1]
+                if ext == '.py':
+                    print(full_filename)
+    except PermisstionError:
+        pass        
+        # print(dirname)
 
 search("C:/")
