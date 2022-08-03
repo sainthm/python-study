a118e5854732e782ba1329299b03bc7d41051d81
sainthm <sainthm@naver.com>
2022-08-04 01:22:02 +0900

    Add sub_dir_search2.py example ver.2(using os.walk)

--- doit/sub_dir_search.2.py
+++ doit/sub_dir_search.2.py
@@ -2,8 +2,11 @@
 
 import os
 
-for (path, dir, files) in os.walk("C:/"):
-    for filename in files:
-        ext = os.path.splitext(filename)[-1]
-        if ext == '.py':
-            print("%s/%s" % (path, filename))
+try:
+    for (path, dir, files) in os.walk("C:/"):
+        for filename in files:
+            ext = os.path.splitext(filename)[-1]
+            if ext == '.py':
+                print("%s/%s" % (path, filename))
+except PermissionError:
+    pass
