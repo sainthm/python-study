fd651535d6b7739f221968c5cdfb5749c4012f9d
sainthm <sainthm@naver.com>
2022-08-26 11:58:01 +0900

    Edit string-1.py file

--- MGS/string-1.py
+++ MGS/string-1.py
@@ -16,10 +16,39 @@
 
 #########################
 
-a = 'HelloWorld!'
+# a = 'HelloWorld!'
 
-print(a[0]+a[5]) 
+# print(a[0]+a[5]) 
+
+# b = 'I Love Futureskill'
+
+# print(b[0]+b[2]+b[7])
+
+
+
+
+#########################
+
+# a = 'HelloWorld!'
+# i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for x in i:
+#     if a[x].isupper() == True:
+#         print(a[x])
+#     else:
+#         pass
 
 b = 'I Love Futureskill'
+print(len(b))
+j = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-print(b[0]+b[2]+b[7])
+for x in j:
+    if b[x].isupper() == True:
+        print(b[x])
+    else:
+        pass
+
+# a[0].isupper()
+
+# print(a[0].isupper())
+# print(a[1].isupper())
