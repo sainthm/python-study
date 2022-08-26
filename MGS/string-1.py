e5a48635b100e1ca7206aeec4ff1557bb2c25f7a
sainthm <sainthm@naver.com>
2022-08-26 12:17:56 +0900

    Edit string-1.py file(Solve #3 & Add #4 question

--- MGS/string-1.py
+++ MGS/string-1.py
@@ -1,3 +1,5 @@
+################################################################################
+
 # 3번 : 대문자만 지나가세요
 
 # 문제
@@ -14,41 +16,82 @@
 # ILF
 
 
-#########################
+# #########################
 
-# a = 'HelloWorld!'
+# # a = 'HelloWorld!'
 
-# print(a[0]+a[5]) 
+# # print(a[0]+a[5]) 
+
+# # b = 'I Love Futureskill'
+
+# # print(b[0]+b[2]+b[7])
+
+
+
+
+# #########################
+
+# # a = 'HelloWorld!'
+# # i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# # for x in i:
+# #     if a[x].isupper() == True:
+# #         print(a[x])
+# #     else:
+# #         pass
 
 # b = 'I Love Futureskill'
+# j = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+# results = ''
 
-# print(b[0]+b[2]+b[7])
-
-
-
-
-#########################
-
-# a = 'HelloWorld!'
-# i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-# for x in i:
-#     if a[x].isupper() == True:
-#         print(a[x])
+# for x in j:
+#     if b[x].isupper() == True:
+#         results += b[x]
 #     else:
 #         pass
 
+# print(results)
+
+a = 'HelloWorld!'
 b = 'I Love Futureskill'
-print(len(b))
+
+i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 j = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-for x in j:
-    if b[x].isupper() == True:
-        print(b[x])
+results_a = ''
+
+results_b = ''
+
+for x in i:
+    if a[x].isupper() == True:
+        results_a += a[x]
     else:
         pass
 
-# a[0].isupper()
+print(results_a)
 
-# print(a[0].isupper())
-# print(a[1].isupper())
+
+for x in j:
+    if b[x].isupper() == True:
+        results_b += b[x]
+    else:
+        pass
+
+print(results_b)
+
+
+
+################################################################################################
+
+# 4번 : 대소문자 바꿔서 출력하기
+# 문제
+# 문자열이 주어지면 대문자와 소문자를 바꿔서 출력하는 프로그램을 작성해주세요.
+
+# (단, 대문자 또는 소문자만 입력으로 들어온다고 가정합니다.)
+
+# Input
+# abC
+# AAABBBcccddd
+# Output
+# ABc
+# aaabbbCCCDDD
